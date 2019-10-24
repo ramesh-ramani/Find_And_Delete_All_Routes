f6c83cad42aa36d69026955977e6e6ec04c37cde
Ramesh Ramani <rramani@salesforce.com>
2019-10-24 13:05:45 -0700

    test-3

--- remove-required-routes.py
+++ remove-required-routes.py
@@ -1,38 +1,41 @@
 import boto3
 import constants
 from netaddr import IPNetwork
+import sys
 
-def route_change(client,src_ip):
+def route_change(client, src_ip):
     """
     Extract the routes and eni information from the AWS API
     """
     for vpc in constants.VPC_FILTER:
-    	response = client.describe_route_tables(Filters=vpc)
+        response = client.describe_route_tables(Filters=vpc)
         for assoc in response['RouteTables']:
             for route in assoc['Routes']:
                 try:
-                    print("Route before change: ",route)
+                    print("Route before change: ", route)
                     if (src_ip in IPNetwork(route['DestinationCidrBlock'])):
-                       response = client.delete_route(
-                       DestinationCidrBlock=route['DestinationCidrBlock'],
-                       RouteTableId=assoc['RouteTableId'])
-                    print("Route after change: ",route)
-                except: continue
- 
+                        response = client.delete_route(
+                            DestinationCidrBlock=route['DestinationCidrBlock'],
+                            RouteTableId=assoc['RouteTableId'])
+                    print("Route after change: ", route)
+                except BaseException:
+                    continue
 
 
 def main():
-    input_cidr = input("Enter the CIDR block you would need to be removed from the relevant VPCs Route Tables: ")
-    while True():
-       try:
-          src_ip = IPNetwork(input_cidr)
-          break
-       except:
-          continue
+    input_cidr = input(
+        "Enter the CIDR block you would need to be removed from the relevant VPCs Route Tables: ")
+    try:
+            src_ip = IPNetwork(input_cidr)
+    except BaseException:
+            print("Incorrect CIDR Block. Exiting!")
+            sys.exit()
+            
     client = boto3.Session(
         profile_name='Riq',
         region_name='us-west-2').client('ec2')
-    route_change(client,src_ip)
+    route_change(client, src_ip)
+
 
 if __name__ == '__main__':
     main()
