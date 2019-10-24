1662fe7dcaa858b9ff0c144652f906781644ca4f
Ramesh Ramani <rramani@salesforce.com>
2019-10-24 12:50:21 -0700

    test-1

--- remove-required-routes.py
+++ remove-required-routes.py
@@ -2,13 +2,13 @@ import boto3
 import constants
 from netaddr import IPNetwork
 
-def route_change(client):
+def route_change(client,src_ip):
     """
     Extract the routes and eni information from the AWS API
     """
     for vpc in constants.VPC_FILTER:
-    response = client.describe_route_tables(
-        Filters=constants.VPC_FILTER)
+    	response = client.describe_route_tables(
+        	Filters=constants.VPC_FILTER)
 
         for assoc in response['RouteTables']:
             for route in assoc['Routes']:
@@ -24,12 +24,17 @@ def route_change(client):
 
 
 def main():
-    input_cidr = ("Enter the CIDR block you would be search/replacing: ")
-    src_ip = IPNetwork(input_cidr)
+    input_cidr = input("Enter the CIDR block you would need to be removed from the relevant VPCs Route Tables: ")
+    while True():
+       try:
+          src_ip = IPNetwork(input_cidr)
+          break
+       except:
+          continue
     client = boto3.Session(
         profile_name='Riq',
         region_name='us-west-2').client('ec2')
-    route_change(client)
+    route_change(client,src_ip)
 
 if __name__ == '__main__':
     main()
