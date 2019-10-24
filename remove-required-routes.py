3b8e3639142312a95578c7f10d026ddd9c845594
Anurag Bhatt <anurag.bhatt@salesforce.com>
2019-10-24 12:53:57 -0700

    More comments, quick pep8 reformat

--- remove-required-routes.py
+++ remove-required-routes.py
@@ -2,37 +2,40 @@ import boto3
 import constants
 from netaddr import IPNetwork
 
-def route_change(client,src_ip):
+
+def route_change(client, src_ip):
     """
     Extract the routes and eni information from the AWS API
     """
     for vpc in constants.VPC_FILTER:
-    	response = client.describe_route_tables(Filters=vpc)
+        response = client.describe_route_tables(Filters=vpc)
         for assoc in response['RouteTables']:
             for route in assoc['Routes']:
                 try:
-                    print("Route before change: ",route)
+                    print("Route before change: ", route)
                     if (src_ip in IPNetwork(route['DestinationCidrBlock'])):
-                       response = client.delete_route(
-                       DestinationCidrBlock=route['DestinationCidrBlock'],
-                       RouteTableId=assoc['RouteTableId'])
-                    print("Route after change: ",route)
-                except: continue
- 
+                        response = client.delete_route(
+                            DestinationCidrBlock=route['DestinationCidrBlock'],
+                            RouteTableId=assoc['RouteTableId'])
+                    print("Route after change: ", route)
+                except:
+                    continue
 
 
 def main():
     input_cidr = input("Enter the CIDR block you would need to be removed from the relevant VPCs Route Tables: ")
+    # This is an infinite loop. Better to just exit?
     while True():
-       try:
-          src_ip = IPNetwork(input_cidr)
-          break
-       except:
-          continue
+        try:
+            src_ip = IPNetwork(input_cidr)
+            break
+        except:
+            continue
     client = boto3.Session(
         profile_name='Riq',
         region_name='us-west-2').client('ec2')
-    route_change(client,src_ip)
+    route_change(client, src_ip)
+
 
 if __name__ == '__main__':
     main()
