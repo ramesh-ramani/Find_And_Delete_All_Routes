628d6d251136836c45933f2739cfe4f59e90ff30
Anurag Bhatt <anurag.bhatt@salesforce.com>
2019-10-25 15:10:04 -0700

    Adding comments

--- remove-required-routes.py
+++ remove-required-routes.py
@@ -15,6 +15,9 @@ def delete_routes(client,routes_to_delete_dict):
 
 
 def route_change(client, src_ip):
+    # Why not just make this a list and then check to see?
+    # Comparing IPNetwork objects is probably not as good an idea as just comparing strings directly.
+
     black_lst_ip_0=IPNetwork("0.0.0.0/0")
     black_lst_ip_10=IPNetwork("10.0.0.0/8")
     routes_to_delete_dict = {}
@@ -29,19 +32,30 @@ def route_change(client, src_ip):
         for assoc in response['RouteTables']:
             for route in assoc['Routes']:
                 try:
+                    # This if condition is really convoluted. Here is what I would suggest:
+                    # if src_ip in blacklisted_ips:
+                    #   continue
+                    # if IPNetwork(src_ip) in IPNetwork(route['DestinationCidrBlock'])):
+                    #   routes_to_delete_dict[route['DestinationCidrBlock']] = assoc['RouteTableId']
                     if (src_ip in IPNetwork(route['DestinationCidrBlock'])) and (IPNetwork(route['DestinationCidrBlock'])!= black_lst_ip_10) and (IPNetwork(route['DestinationCidrBlock'])!= black_lst_ip_0):
                         print("Route before change: ", route, ",VPC ID: ", vpc['Values'][0], ",Route Table ID: ", assoc['RouteTableId'])
+                        # Are the dictionary items supposed to be lists?
                         routes_to_delete_dict[route['DestinationCidrBlock']] = [assoc['RouteTableId']]
                     #print("Route after change: ", route)
                 except:
                     continue
 
+        # This is where using something like click.confirm() would be amazing. You don't need to do this stuff.
+        # Also, user_response should really just compare the first letter. So:
+        # if user_response[0].lower() == 'y':
+        #   etc
         while True:
             print("These are the routes that will be deleted: \n")
             user_response = input("Are you sure you want to proceed with the change?(Y/N): ")
             user_response = user_response.lower()
             if user_response =='y':
                print("Hold on. Deleting routes!")
+               # time is not being imported
                time.wait(30)
                break
             elif user_response =='n':
