15664fd3b9420ff46586f4830ff9bb95b293a9aa
Ramesh Ramani <rramani@salesforce.com>
2019-10-25 16:13:20 -0700

    successfull remove case tested

--- remove-required-routes.py
+++ remove-required-routes.py
@@ -2,7 +2,7 @@ import boto3
 import constants
 from netaddr import IPNetwork
 import sys
-
+import time
 
 
 def delete_routes(client,routes_to_delete_dict):
@@ -10,18 +10,22 @@ def delete_routes(client,routes_to_delete_dict):
         response = client.delete_route(
              DestinationCidrBlock=DestinationCidrBlock,
              RouteTableId=RouteTableId)
-        print("Route Deleted: ",reponse)
+        print("Route Deleted: ",response)
     print("Routes Deleted! Exiting!")
 
 
 def route_change(client, src_ip):
-    black_lst_ip_0=IPNetwork("0.0.0.0/0")
-    black_lst_ip_10=IPNetwork("10.0.0.0/8")
+    black_lst_ips = [
+        IPNetwork("0.0.0.0/0"),
+        IPNetwork("10.0.0.0/8")
+    ]
     routes_to_delete_dict = {}
  
     """
     Extract the routes and eni information from the AWS API
     """
+    
+           
     for vpc in constants.VPC_FILTER:
         routes_to_delete_dict={}
         response = client.describe_route_tables(Filters=[vpc])
@@ -29,7 +33,10 @@ def route_change(client, src_ip):
         for assoc in response['RouteTables']:
             for route in assoc['Routes']:
                 try:
-                    if (src_ip in IPNetwork(route['DestinationCidrBlock'])) and (IPNetwork(route['DestinationCidrBlock'])!= black_lst_ip_10) and (IPNetwork(route['DestinationCidrBlock'])!= black_lst_ip_0):
+                    bool_list = [IPNetwork(route['DestinationCidrBlock']) == i for i in black_lst_ips]
+                    if any(bool_list):
+                        continue
+                    if (src_ip in IPNetwork(route['DestinationCidrBlock'])):
                         print("Route before change: ", route, ",VPC ID: ", vpc['Values'][0], ",Route Table ID: ", assoc['RouteTableId'])
                         routes_to_delete_dict[route['DestinationCidrBlock']] = [assoc['RouteTableId']]
                     #print("Route after change: ", route)
@@ -37,17 +44,15 @@ def route_change(client, src_ip):
                     continue
 
         while True:
-            print("These are the routes that will be deleted: \n")
             user_response = input("Are you sure you want to proceed with the change?(Y/N): ")
-            user_response = user_response.lower()
-            if user_response =='y':
+            if user_response[0].lower() == 'y':
                print("Hold on. Deleting routes!")
                time.wait(30)
                break
-            elif user_response =='n':
+            elif user_response[0].lower() == 'n':
                print("Ok. Not making changes for this VPC")
                break
-        #delete_routes(client,routes_to_delete_dict)
+        delete_routes(client,routes_to_delete_dict)
 
 
 
