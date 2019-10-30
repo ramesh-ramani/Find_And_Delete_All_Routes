6a0a27093d57aeee3e262ac4abccd2fba6162859
Ramesh Ramani <rramani@salesforce.com>
2019-10-30 13:02:09 -0700

    added roll back feature and modified Readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+Details:
+======
+
 This Script takes VPC IDs as the input (in the constants.py file).
 
 The Script then takes the user input for the CIDR blocks to search for (to Delete from the above VPC IDs), looks into the Route Tables for the VPCs and deletes the relevant Routes
+
+Output:
+=====
+
+1. It will print out the details of the matching routes and check with you if you would like to make route changes. If yes, it will Delete the routes
+2. If not, it will exit the Loop
+3. Once the Routes are Deleted, it will check if you would like to revert the changes. If yes, it will revert all the changes. If not, the program will exit
+
--- remove-required-routes.py
+++ remove-required-routes.py
@@ -3,17 +3,37 @@ import constants
 from netaddr import IPNetwork
 import sys
 import time
+import json
+
+routes_before_change = []
+
+def revert_route_change(client):
+    print("Going to revert Changes!")
+    for i in routes_before_change:
+        response = client.create_route(
+            DestinationCidrBlock=i[0],
+            VpcPeeringConnectionId=i[1],
+            RouteTableId=i[3]
+        )
+    print("Details of revert: ",i)
+    print(response)
 
 
 def delete_routes(client,routes_to_delete_dict):
     for DestinationCidrBlock,RouteTableId in routes_to_delete_dict.items():
         response = client.delete_route(
              DestinationCidrBlock=DestinationCidrBlock,
-             RouteTableId=RouteTableId)
+             RouteTableId=RouteTableId[0])
         print("Route Deleted: ",response)
-    print("Routes Deleted! Exiting!")
-
-
+    print("Routes Deleted!")
+    revert_routes_check = input("\nDo do you want to revert your change?(Y/N): ") 
+    while True():
+        if revert_routes_check[0].lower() == 'n':
+           print("Exiting!")
+           sys.exit()
+        elif revert_routes_check[0].lower() == 'y':
+           revert_route_change(client)      
+    
 def route_change(client, src_ip):
 
     black_lst_ips = [
@@ -40,9 +60,9 @@ def route_change(client, src_ip):
                     bool_list = [IPNetwork(route['DestinationCidrBlock']) == i for i in black_lst_ips]
                     if any(bool_list):
                         continue
-                    if (src_ip == IPNetwork(route['DestinationCidrBlock'])):
+                    if (src_ip == IPNetwork(route['DestinationCidrBlock']) or IPNetwork(route['DestinationCidrBlock']) in src_ip):
+                        routes_before_change.append([route['DestinationCidrBlock'],route['VpcPeeringConnectionId'],vpc['Values'][0],assoc['RouteTableId']])
                         print("Route before change: ", route, ",VPC ID: ", vpc['Values'][0], ",Route Table ID: ", assoc['RouteTableId'])
-                        # Are the dictionary items supposed to be lists?
                         routes_to_delete_dict[route['DestinationCidrBlock']] = [assoc['RouteTableId']]
                     #print("Route after change: ", route)
                 except:
@@ -57,11 +77,11 @@ def route_change(client, src_ip):
                print("Hold on. Deleting routes!")
                # time is not being imported
                time.sleep(30)
+               delete_routes(client,routes_to_delete_dict)
                break
             elif user_response[0].lower() == 'n':
                print("Ok. Not making changes for this VPC")
                break
-#        delete_routes(client,routes_to_delete_dict)
 
 
 
